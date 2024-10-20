42f4b71fc073a8593ae27d15e05f0ab2588dd402
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-10-20 23:38:24 +1100

    basic packet capture loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use pcap::{Device, Capture};
+
 fn main() {
-    println!("Hello, world!");
+    for device in pcap::Device::list().expect("device lookup failed") {
+        println!("Found device! {:?}", device);
+    }
+
+    let mut cap = pcap::Capture::from_device("en0")
+    .unwrap()
+    .immediate_mode(true)
+    .open()
+    .unwrap();
+
+    cap.filter("src host 192.168.8.1", true).unwrap();
+
+    while let Ok(packet) = cap.next_packet() {
+        
+        println!("got packet! {:?}", packet)
+    }
 }
